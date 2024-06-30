10d2e63bfd6497736392d37bd5b6aa2cfdc19293
sarahvill <130417177+sara-villagra@users.noreply.github.com>
2024-06-30 15:49:05 -0300

    agregué función para rutas protegidas

--- index.js
+++ index.js
@@ -30,8 +30,32 @@ app.use(
 app.use(methodOverride('_method'))
 
 let usuarios = [] // base de datos de usuarios simulada
-let tareas = [] // base de datos de tareas simulada
+let tareas = [
+ {
+  id: 1,
+  name: 'tarea 1',
+  descripcion: 'Esta es la descripcion 1'
+ },
+ {
+  id: 2,
+  name: 'tarea 2',
+  descripcion: 'Esta es la descripcion 2'
+ },
+ {
+  id: 3,
+  name: 'tarea 3',
+  descripcion: 'Esta es la descripcion 3'
+ }
+] // base de datos de tareas simulada
+//funcion para autentificar rutas
 
+const authenticateUser = (req, res, next) => {
+ if (req.session.user) {
+  next()
+ } else {
+  res.redirect('/login')
+ }
+}
 // Ruta para mostrar el formulario de registro
 app.get('/register', (req, res) => {
  res.render('register')
@@ -54,18 +78,43 @@ app.get('/login', (req, res) => {
 })
 
 // Ruta para manejar el inicio de sesión de usuarios
-app.post('/login', (req, res) => {})
+app.post('/login', (req, res) => {
+ // Verificar que el nombre de usuario y contraseña existan y sean correctos
+ const { username, password } = req.body
+ const user = usuarios.find(
+  (u) => u.username === username && u.password === password
+ )
+ if (!user) {
+  res.render('login') // Si el usuario está autenticado
+ } else {
+  req.session.user = user
+  res.render('tareas', { tareas }) // Si el usuario no está autenticado
+ }
+})
 
 // Ruta para manejar el cierre de sesión de usuarios
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+ req.session.destroy()
+ res.send('Usuario deslogueado')
+})
 
 // Ruta para mostrar las tareas (solo si el usuario está autenticado)
-app.get('/tareas', (req, res) => {
+app.get('/tareas', authenticateUser, (req, res) => {
  res.render('tareas', { tareas }) // Si el usuario está autenticado
 })
 
 // Ruta para manejar la creación de nuevas tareas (solo si el usuario está autenticado)
-app.post('/tareas', (req, res) => {})
+app.post('/tareas', authenticateUser, (req, res) => {
+ // Agregar la nueva tarea a la base de datos
+ const { name, descripcion } = req.body
+ tareas.push({
+  id: tareas.length + 1,
+  name,
+  descripcion
+ })
+ //res.send('Tarea agregada correctamente')
+ res.render('tareas', { tareas })
+})
 
 // Ruta para manejar la eliminación de tareas (solo si el usuario está autenticado)
 app.delete('/tareas/:id', (req, res) => {})
